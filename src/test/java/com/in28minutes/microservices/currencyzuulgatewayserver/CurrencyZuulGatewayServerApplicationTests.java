d82ad6bda730db26ecf8860871255be60ff8ac7e
ardi priasa <ardi.priasa@gmail.com>
2019-10-04 15:16:45 +0700

    import source code

---
+++ src/test/java/com/in28minutes/microservices/currencyzuulgatewayserver/CurrencyZuulGatewayServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.in28minutes.microservices.currencyzuulgatewayserver;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class CurrencyZuulGatewayServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
